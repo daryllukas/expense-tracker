81477d66f1eb961fec3cd189014c0b57502d7484
Daryl Lukas <hello@daryllukas.me>
2021-06-02 09:35:29 +0200

    feat: add register, login and logout methods to service class

--- src/services/AppwriteService.js
+++ src/services/AppwriteService.js
@@ -10,6 +10,20 @@ const appwrite = new Appwrite();
 class AppwriteService {
   constructor() {
     appwrite.setEndpoint(config.endpoint).setProject(config.projectId);
+
+    this.account = appwrite.account;
+  }
+
+  doCreateAccount = (email, password, name) => {
+      return this.account.create(email, password, name);
+  }
+
+  doLogin = (email, password) => {
+      return this.account.createSession(email, password);
+  }
+
+  doLogout = () => {
+      return this.account.deleteSession('current');
   }
 }
 
