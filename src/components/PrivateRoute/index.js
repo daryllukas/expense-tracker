ff512089c6cf40dc38bb565aec6818b6b7689d85
Daryl Lukas <hello@daryllukas.me>
2021-06-26 12:26:20 +0200

    refactor: remove useless useeffect hook

--- src/components/PrivateRoute/index.js
+++ src/components/PrivateRoute/index.js
@@ -1,23 +1,24 @@
 import React, { useEffect, useContext } from 'react';
 import AuthUserContext from '../../context/Session';
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-
-    const { authUser, getCurrentUser } = useContext(AuthUserContext);
-
-    useEffect(() => {
-        getCurrentUser();
-    }, [])
-
-    return (
-        <Route {...rest}
-            render={props => authUser ?
-                (<Component {...props} />) :
-                (<Redirect to={{ pathname: ROUTES.SIGN_IN, state: { from: props.location } }} />)}
-        />
-    )
-}
+  const { authUser } = useContext(AuthUserContext);
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        authUser ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: ROUTES.SIGN_IN, state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+};
 
 export default PrivateRoute;
